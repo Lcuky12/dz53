e7a0db54a6515d966ab2c4bf248b23915a68cf99
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2023-03-09 21:21:40 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp49
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int minNumber = 1;
+            int maxNumber = 28;
+            int threeDigitNumber = 999;
+            int numberRandom;
+
+            numberRandom = random.Next(minNumber, maxNumber);
+
+            for ( int i = 0; i <= threeDigitNumber; i+=numberRandom)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
